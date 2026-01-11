3094e111075c8af40ca7fd4cd2698efdb51917a0
FengCastelo <ianfcastelo7@gmail.com>
2026-01-11 12:35:32 -0300

    Random port to worker

--- hr-payroll/src/main/java/com/fengcastelo/hr_payroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/fengcastelo/hr_payroll/feignclients/WorkerFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "hr-worker", url = "http://localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
     @GetMapping("/{id}")
     Worker findById(@PathVariable("id") Long id);
