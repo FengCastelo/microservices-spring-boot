4422b3559cba78a6b93b0d6235585c844546ff28
FengCastelo <ianfcastelo7@gmail.com>
2026-01-18 23:47:05 -0300

    Random Port to hr-payroll

--- hr-payroll/src/main/java/com/fengcastelo/hr_payroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/fengcastelo/hr_payroll/feignclients/WorkerFeignClient.java
@@ -6,9 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
+
     @GetMapping("/{id}")
     Worker findById(@PathVariable("id") Long id);
 }
--- hr-worker/src/main/java/com/fengcastelo/hr_worker/Resources/WorkerResource.java
+++ hr-worker/src/main/java/com/fengcastelo/hr_worker/Resources/WorkerResource.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class WorkerResource implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+    private static final Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
     @Autowired
     private Environment env;
