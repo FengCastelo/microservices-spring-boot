b1bffc0a2b49adc4946d076b3bbde20074b5a760
FengCastelo <ianfcastelo7@gmail.com>
2026-01-21 13:19:35 -0300

    Authorization Server created

---
+++ ms-oauth/src/test/java/com/fengcastelo/ms_oauth/MsOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengcastelo.ms_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
